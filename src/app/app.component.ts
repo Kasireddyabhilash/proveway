efb56838ee1f289dae5a96e1b377e14670f060f7
kasireddyabhilashreddy <kasireddyabhilashreddy@gmail.com>
2024-11-08 16:48:33 +0530

    proveway

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Task';
+  selectedOfferId: number | null = null;  // Tracks which offer is selected
+  totalPrice: number = 0;
+
+  // Function to select an offer and set the total price
+  selectOffer(offerId: number, price: number) {
+    this.selectedOfferId = offerId;
+    this.totalPrice = price;
+  }
+
+  // Function to format the total price as a currency
+  getFormattedPrice() {
+    return `DKK ${this.totalPrice.toFixed(2)}`;
+  }
+
 }
